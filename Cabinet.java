5295bce358ca39bda915e081b441249fd24f1b28
HyunA-PC\HyunA <khyun9764@gmail.com>
2016-11-13 01:22:40 +0900

    sql문 추가한 "캐비넷"관련 클래스 작성 및 데이터베이스 관련 함수 재작성 - 김현아

--- Cabinet.java
+++ Cabinet.java
@@ -1,33 +1,38 @@
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager; 
+import java.sql.ResultSet; 
+import java.sql.SQLException;
+import java.sql.Statement; 
 
 class Cabinet {
-
+	database db= new database();
 	public int cabinet_number=0;
 	public int cabinet_state[]=new int [100];
 	public int input_verification_number;
-	int assign_cabinet(int veri_num)
+	int assign_cabinet(int veri_num, int printer_loc) throws SQLException
 	{
-		int cabinet_state[]=new int [100];
 		int i=0;
 		//DB에서 cabinet의 상태를 가져옴
 		//DB에서 하나하나 검사하다가 0인 부분이 있다면 for문을 나오게 함. 
-		for(i=0;i<100;i++)
-		{
-			if(cabinet_state[i]==0)
-				{
-					cabinet_number=i;
-					//DB에 veri_num을 저장함. 
-					//DB에서 그 번호의 상태를 1으로 바꿈. 
-					break;
-				}
-		}
-		if(i==100)
+		cabinet_number=i;
+		db.database_use();
+		String sql="select cabinet_number from cabinet_tbl where cabinet_state = 0 and printer_loc = "+printer_loc;
+		db.rs=db.stmt.executeQuery(sql);
+		cabinet_number=db.rs.getInt("cabinet_number");
+		if(db.rs==null)
 		{
 			//모두 full이면 배정받을 사물함이 없음을 출력함-> 사물함을 관리자가 강제로 비우라는 메세지를 출력함. return -1하고 끝남. 
 			System.out.println("빈 사물함이 없습니다. 사물함을 비우세요!");
 			return -1;
 		}
+		//DB에 veri_num을 저장함. 
+		//DB에서 그 번호의 상태를 1으로 바꿈. 
+		sql="update cabinet set cabinet_state = "+1+" and Order_Account_Order_ID = "+veri_num+"where cabinet_number = "+ cabinet_number+"and Printer_loc"+printer_loc;
+		db.stmt.executeQuery(sql);
 		//하나라도 empty라면 그냥 번호 빠른 순으로 배정하도록함. 
 		//해당 번호를 return해줌. 
 		return cabinet_number;
 	}
+	
 }
--- Manager_Interface.java
+++ Manager_Interface.java
@@ -1,33 +1,37 @@
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Manager_Interface {
-
+	database db= new database();
 	public Cabinet cabinet;
 	public void manage_printer()
 	{
 		//프린터의 상태를 가져옴.
 		//큐에 있는 내용을 삭제할 수있음. 
 	}
-	public void manage_cabinet()
+	public void manage_cabinet() throws SQLException
 	{
 		cabinet = new Cabinet();
 		Scanner in=new Scanner(System.in);
 		//UI로 인증번호를 입력하라고 함. 
 		int veri_num;
+		int printer_loc;
+		System.out.println("캐비넷의 장소를 선택하세요!");
+		printer_loc=in.nextInt();
+		
 		System.out.println("인증번호를 입력하세요.");
 		veri_num=in.nextInt();
 		//입력한 인증번호를 매개변수로 넣어 호출함. 
-		cabinet.assign_cabinet(veri_num);
+		cabinet.assign_cabinet(veri_num,printer_loc);
 	}
-	private void show_cabinet_num(int num)
+	private void show_cabinet_num(int num) throws SQLException
 	{
 		//DB에서 캐비넷 상태를 가져옴. 
 		//캐비넷의 상태를 출력함. 
-		int i=0;
-		for(i=0;i<100;i++)
-		{
-			System.out.println(cabinet.cabinet_state[i]);
-		}
+		db.database_use();
+		String sql="select cabinet_number from cabinet where Order_Account_Order_ID = "+ num;
+		db.rs=db.stmt.executeQuery(sql);
+		System.out.println("cabinet_number: " + db.rs.getInt("cabinet_number")); // Data get 
 		
 	}
 }

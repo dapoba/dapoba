604a77b0c04dd88eb24114eaaa1c3ebcee0cf6f1
HyunA-PC\HyunA <khyun9764@gmail.com>
2016-11-22 00:47:40 +0900

    간단한 프린터 체크 기능을 넣은 Printer.java -김현아

--- Printer.java
+++ Printer.java
@@ -1,22 +1,129 @@
+package test;
 
-public class Printer {
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.print.DocPrintJob;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import javax.print.ServiceUIFactory;
+import javax.print.attribute.Attribute;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.PrintServiceAttributeSet;
+import javax.print.attribute.standard.Chromaticity;
+import javax.print.attribute.standard.Copies;
+import javax.print.attribute.standard.MediaSizeName;
+import javax.print.attribute.standard.Sides;
+/**
+ * Servlet implementation class Printer
+ */
+@WebServlet("/Printer")
+public class Printer extends HttpServlet {
 
-	public File result_file;
-	public int verification_number;
-	public File get_order_file[20];
-	public int[] get_printer_states(Option option)
+	/**
+	protected PrintService[] m_services;
+	protected PrintService m_serviceDef = null;
+	m_services = PrinterJob.lookupPrintSErvices();
+	m_serviceDef=PrintServiceLookup.lookupDefaultPrintService();
+	**/
+	public static void main(String[] args)
 	{
-		int a[]=new int [20];
-		return a;
+		new Printer().info();
+		try{
+			PrinterJob job=PrinterJob.getPrinterJob();
+			PrintRequestAttributeSet aset=new HashPrintRequestAttributeSet();
+			job.print(aset);
+		}
+		catch(PrinterException ex)
+		{
+			
+		}
 	}
-	public boolean check_availability(Printer printer)
+	public void info()
 	{
-		return true;
+		PrintService svc=PrintServiceLookup.lookupDefaultPrintService();
+		info(svc);
+		PrintService[] services=PrintServiceLookup.lookupPrintServices(null, null);
+		/**
+		 * 
+		 * for(int i=0;i<services.length;i++)
+		 * {
+		 * PrintService service=services[i];
+		 * info(service);
+		 * }
+		 */
+		PrintRequestAttributeSet attrs=new HashPrintRequestAttributeSet();//null이 아니어야함. 
+		attrs.add(MediaSizeName.TABLOID);
+		attrs.add(MediaSizeName.TABLOID); // paper size  http://java.sun.com/j2se/1.5.0/docs/api/javax/print/attribute/standard/MediaSizeName.html
+		attrs.add(Chromaticity.MONOCHROME); // 흑백인쇄 http://java.sun.com/j2se/1.5.0/docs/api/javax/print/attribute/standard/Chromaticity.html#MONOCHROME
+		attrs.add(Sides.DUPLEX); // 양면인쇄  http://java.sun.com/j2se/1.5.0/docs/api/javax/print/attribute/standard/Sides.html#DUPLEX
+
+		//PrintService selection = ServiceUI.printDialog(  null, 100, 100, services, svc, null, attrs);
+		PrintService defaultService = PrintServiceLookup.lookupDefaultPrintService();
+		DocPrintJob printJob = defaultService.createPrintJob();
 	}
-	public boolean print_bill(String bill)
+	void info(PrintService s)
 	{
-		return true;
+	 System.out.println(s.getName() + " :: "+s.getClass().getName());
+	 PrintServiceAttributeSet set = s.getAttributes();
+	 showAttributes(set);
+	 ServiceUIFactory fac = s.getServiceUIFactory();
+	 System.out.println("fac: "+fac); // null
+    }
+
+	void showAttributes(PrintServiceAttributeSet set)
+	{
+		System.out.println("set: "+set); // javax.print.attribute.AttributeSetUtilities$UnmodifiablePrintServiceAttributeSet@85028e19
+		Attribute[] attrs = set.toArray();
+		for (int i=0;i<attrs.length;i++)
+		{
+			Attribute attr = attrs[i];
+			System.out.println("attr: "+attr.getName()+" "+attr.toString()+" "+attr.getCategory());
+		}
+		/*
+		 *  attr: printer-is-accepting-jobs accepting-jobs
+	        attr: printer-name Samsung CLP-300 Series
+	        attr: color-supported supported
+	        attr: queued-job-count 0
+		 * */
 	}
-	private int waiting_queue[]=new int [100];
-	private boolean print_file(Order order);
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Printer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }

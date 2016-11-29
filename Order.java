4dd621721f80c0ce11b1a0617d2eb84f57c9bafc
DESKTOP-6MHFUCU\Hyojin <gywlsdl312@gmail.com>
2016-11-30 08:51:40 +0900

    Order.java에서 Milege_Coin.java로 넘어온 함수들이 있어 제외하였습니다.-남효진

--- Order.java
+++ Order.java
@@ -1,36 +1,16 @@
 
 public class Order {
 
-	public int verification_number;
 	public File order_detail;
-	public String bill;
-	public void issue_bill(Milege_Coin milege_coin)
-	{
-		
-	}
-	public void issue_verification_number()
-	{
-		
-	}
+	
 	public void sent_result_file_to_printer(File file)
 	{
 		
 	}
-	public void store_order_list()
-	{
-		
-	}
+	
 	public void reservation_file()
 	{
 		
 	}
-	public int rand()
-	{
-		return 1;
-	}
-	public String[] get_ordered_list()
-	{
-		String a[]=new String [100];
-		return a;
-	}
+	
 }

db7f160cd4f2ce6493e06b143c15bbfeccf8155d
DESKTOP-6MHFUCU\Hyojin <gywlsdl312@gmail.com>
2016-11-30 08:53:34 +0900

    database.java 결제내역 조회 등 추가 구현 - 남효진

--- database.java
+++ database.java
@@ -2,7 +2,9 @@ import java.sql.Connection;
 import java.sql.DriverManager; 
 import java.sql.ResultSet; 
 import java.sql.SQLException;
-import java.sql.Statement; 
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date; 
 public class database 
 { 
 	static String driver = "com.mysql.jdbc.Driver"; 
@@ -40,33 +42,37 @@ public class database
 		catch(ClassNotFoundException | SQLException e) { e.printStackTrace(); }
 		return c;
 	}
-	static void database_milege(String id, int milege){
+	static void database_milege_coin(String id, String type, int payment){
 		try{
 			Class.forName(driver);
 			conn = DriverManager.getConnection(url, userId, passwd);// Connection 
 			String sql = "Use dapoba_database";
 			stmt = conn.createStatement();
 			stmt.execute(sql);
-			sql = "update Mileage/Coin set Milage = "+String.valueOf(milege)+"where ID = "+id;
+			sql = "update Mileage/Coin set"+type+" = "+String.valueOf(payment)+"where ID = "+id;
 			rs = stmt.executeQuery(sql);
 			stmt.close(); 
 			conn.close(); // close
 		}
 		catch(ClassNotFoundException | SQLException e) { e.printStackTrace(); }
 	}
-	static void database_coin(String id, int coin){
+	static int[] database_get_milege_coin(String id){
 		try{
+			int[] milege_coin = new int[2];
 			Class.forName(driver);
 			conn = DriverManager.getConnection(url, userId, passwd);// Connection 
 			String sql = "Use dapoba_database";
 			stmt = conn.createStatement();
 			stmt.execute(sql);
-			sql = "update Mileage/Coin set Coin = "+String.valueOf(coin)+"where ID = "+id;
+			sql = "select * from Mileage/Coin where id = " + id;
 			rs = stmt.executeQuery(sql);
+			milege_coin[0] = rs.getInt("Coin");
+			milege_coin[1] = rs.getInt("Mileage");
 			stmt.close(); 
-			conn.close(); // close
+			conn.close();// close
+			return milege_coin;
 		}
-		catch(ClassNotFoundException | SQLException e) { e.printStackTrace(); }
+		catch(ClassNotFoundException | SQLException e) { e.printStackTrace(); return null;}
 	}
 	static boolean database_check_verification(int num){
 		boolean flag=true;
@@ -82,6 +88,8 @@ public class database
 			{ 
 				if(num == rs.getInt("verification_number"))
 					flag = false; 
+				if(num <1000)
+					flag = false;
 			} 
 			stmt.close(); 
 			conn.close(); // close
@@ -90,37 +98,104 @@ public class database
 	
 		return flag;
 	}
-	
-	
-	
-	
-	
-	
-	static void database_func()  
-	{ 
-		
-		try
-		{ 
-			Class.forName(driver); // Driver Loading 
+	static boolean database_card_info(int cardNum, String bankName, String date, int password){
+		boolean flag;
+		try{
+			Class.forName(driver);
 			conn = DriverManager.getConnection(url, userId, passwd);// Connection 
-			String sql = "CREATE DATABASE my_database"; // SQL ÀÛ¼º 
-			stmt = conn.createStatement(); 	// Statement 
-			stmt.execute(sql); 
-			sql = "USE my_database"; 
-			stmt.execute(sql); 
-			sql = "create table my_table ( id int, first_name varchar(20), email varchar(30) )"; 
-			stmt.execute(sql); 
-			sql = "insert into my_table (id, first_name, email) values " + "(1, \"woongjin\", \"finalboogi@naver.com\"), " + "(2, \"woongjin2\", \"finalboogi2@naver.com\"), " + "(3, \"woongjin3\", \"finalboogi3@naver.com\")"; 
-			stmt.execute(sql); 
-			sql = "select * from my_table";
-			rs = stmt.executeQuery(sql);// ResultSet 
+			String sql = "Use dapoba_database";
+			stmt = conn.createStatement();
+			stmt.execute(sql);
+			sql = "select * from Credit_Card where Credit_card_number = " + String.valueOf(cardNum);
+			rs = stmt.executeQuery(sql);
+			
+			if(rs.next() == false)
+				flag = false;
+			else{
+				if(bankName == rs.getString("Type") && date == rs.getString("ExpDate") && password == rs.getInt("Credit_card_password"))
+					flag = true;
+				else
+					flag = false;
+			}
+			stmt.close(); 
+			conn.close(); // close
+		}
+		catch(ClassNotFoundException | SQLException e) { e.printStackTrace(); flag = false; }
+	
+		return flag;
+	}
+	static boolean database_card_balance(int cardNum, int money){
+		boolean flag;
+		try{
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, userId, passwd);// Connection 
+			String sql = "Use dapoba_database";
+			stmt = conn.createStatement();
+			stmt.execute(sql);
+			sql = "select Balance from Credit_Card where Credit_card_number = " + String.valueOf(cardNum);
+			rs = stmt.executeQuery(sql);
+			rs.next();
+			int balance = rs.getInt("Balance");
+			if(money <= balance){
+				flag = true;
+				balance -= money;
+				sql = "update Credit_Card set Balance = "+String.valueOf(balance)+"where Credit_card_number = "+String.valueOf(cardNum);
+				rs = stmt.executeQuery(sql);
+			}
+			else
+				flag = false;
+			stmt.close(); 
+			conn.close(); // close
+		}
+		catch(ClassNotFoundException | SQLException e) { e.printStackTrace(); flag = false; }
+	
+		return flag;
+	}
+	static void database_history_insert(String id, String type, int transaction, String file){
+		try{
+			long time = System.currentTimeMillis(); 
+			SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+			String date = dayTime.format(new Date(time));
+
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, userId, passwd);// Connection 
+			String sql = "Use dapoba_database";
+			stmt = conn.createStatement();
+			stmt.execute(sql);
+			if(type.equals("coin"))
+				sql = "insert into history values (" + id + ", "+ date + ", " +String.valueOf(transaction) +", "+0+", " +file+")";
+			else if(type.equals("milege"))
+				sql = "insert into history values (" + id + ", " + date + ", "+0+", " + String.valueOf(transaction) + ", "+file +")";
+			rs = stmt.executeQuery(sql);			
+			stmt.close(); 
+			conn.close(); // close
+		}
+		catch(ClassNotFoundException | SQLException e) { e.printStackTrace();}
+	}
+	static String[][] database_history_print(String id){
+		try{
+			String[][] history = new String[10][4]; 
+			int i = 0;
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, userId, passwd);// Connection 
+			String sql = "Use dapoba_database";
+			stmt = conn.createStatement();
+			stmt.execute(sql);
+			sql = "select *from history where Account_ID = " + id;
+			rs = stmt.executeQuery(sql);
+			rs.next();
 			while(rs.next()) 
 			{ 
-				System.out.println("id: " + rs.getInt("id") + "\nfirst_name: " + rs.getString("first_name") + "\nemail: " + rs.getString("email") + "\n"); // Data get 
+				history[i][0] = rs.getString("date");
+				history[i][1] = rs.getString("Coin_history");
+				history[i][2] = rs.getString("Mileage_history");
+				history[i][3] = rs.getString("File_name");
+				i++;
 			} 
 			stmt.close(); 
 			conn.close(); // close
-		} 
-		catch(ClassNotFoundException | SQLException e) { e.printStackTrace(); }
-	} 
+			return history;
+		}
+		catch(ClassNotFoundException | SQLException e) { e.printStackTrace(); return null;}
+	}
 }

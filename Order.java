1aa8974e8cbc275dc06cf0c5e174e494f1c2c3c8
DESKTOP-6MHFUCU\Hyojin <gywlsdl312@gmail.com>
2016-12-01 16:45:25 +0900

    Order.java 일부 구현 - 남효진

--- Order.java
+++ Order.java
@@ -1,20 +1,26 @@
-public class Order {
+public class Order extends Account{
 
 	public File order_detail;
 	
+	//프린트로 넘김
 	public void sent_result_file_to_printer(File file)
 	{
-		
+		//큐로 보내기
+		//확장자명, 파일명 보내기
 	}
 	
 	public void reservation_file()
 	{
 		
 	}
-	public void send_option(Option option){
-		Milege_Coin mc = new Milege_Coin();
-		mc.option = option;
-		
-	}
 	
+	//파일에서 받아옴, 결제로 넘김
+	public void send_option(Option option){
+		order_detail = option.fileName;
+		Milege_Coin mc = new Milege_Coin();
+		mc.get_Milege_Coin(super.ID, String.valueOf(super.phone_number), option);
+		RequestDispatcher rd;
+		rd = getServeltContext().getRequestDispatcer("/payment-info.jsp");
+		rd = forward(request, response);
+	}
 }

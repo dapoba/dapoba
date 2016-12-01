826e2b832f372168c3b5d30f1b5f49adcbb88c75
YiJaeHyuk <capree1221@gmail.com>
2016-12-01 15:35:37 +0900

    id_Find.jsp 와 pw_Fin.jsp 파일과 연동되어 서버로부터 아이디와 비밀번호를 찾아주는 서블릿 이재혁

---
+++ id_Find.java
@@ -0,0 +1,90 @@
+package dsa;
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+import java.sql.ResultSet; 
+import java.sql.SQLException; 
+import java.sql.Statement; 
+import java.io.IOException;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FormEx
+ */
+@WebServlet("/id_Find")
+public class id_Find extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public id_Find() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+
+
+    
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//***************************************************************//
+		String driver = "com.mysql.jdbc.Driver"; 
+		String url = "jdbc:mysql://127.0.0.1:3306/dapoba_db?autoReconnect=true&useSSL=false"; 
+		String userId = "root"; 
+		String passwd = "1234"; 
+		String table = "account";
+		Connection conn; 
+		Statement stmt; 
+		//**************************************************************//
+		try { 
+			Class.forName(driver); // Driver Loading 
+			conn = DriverManager.getConnection(url, userId, passwd);// Connection 
+			stmt = conn.createStatement();
+
+			StringBuilder sb = new StringBuilder();
+		
+			String name = request.getParameter("name");
+			String Phone_number = request.getParameter("phone_number");
+			 
+	
+			String sql = sb.append("select ID from " + table + " where (name like '" + name + "') and (phone_number like '" + Phone_number + "');" ).toString();
+		
+			System.out.println(sql);
+			try{
+				ResultSet rs = stmt.executeQuery(sql);
+				if(rs.next()== true)//중복이 없다면 참 값 반환, 있다면 거짓 값 반환.
+					System.out.println( rs.getString("ID"));
+				
+				else
+					System.out.println("ID does not exist");
+						
+			} catch (SQLException e)
+			{
+				//TODO Aito-generated catch block
+				e.printStackTrace();
+			}
+			
+		
+			} catch(ClassNotFoundException | SQLException e) { e.printStackTrace(); 
+		}	
+	}
+}
